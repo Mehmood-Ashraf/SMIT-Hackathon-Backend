22b11be93d3ad10555e00fc3d29e08dd2732139e
Mehmood M.Ashraf <mehmoodfazlani@gmail.com>
2025-08-07 07:09:23 +0500

    "express mongo sanitize middleware Implemented"

--- api/index.js
+++ api/index.js
@@ -1,36 +1,29 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
-import authRoutes from '../routes/authRoutes.js'
-import userRoutes from "../routes/userRoutes.js"
-import { connectDB } from '../connectDB/connectDB.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
-import path from "path"
-import { fileURLToPath } from 'url';
+import authRoutes from "../routes/authRoutes.js";
+import userRoutes from "../routes/userRoutes.js";
+import { connectDB } from "../connectDB/connectDB.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoSanitize from "express-mongo-sanitize";
 
-
-const app = express();
 dotenv.config();
-
-// const _filename = fileURLToPath(import.meta.url)
-// const _dirname = path.dirname(_filename)
-
-const port = process.env.PORT
-connectDB()
-
-app.use(cors({
-    origin : 'http://localhost:5173',
-    credentials : true
-}
-))
-// app.use("/uploads", express.static(path.join(_dirname, "public/uploads")));
+const app = express();
+connectDB();
 app.use(express.json());
-app.use(cookieParser())
-app.use('/api/auth', authRoutes)
-app.use('/api/users', userRoutes)
+app.use(mongoSanitize());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
-
+const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
